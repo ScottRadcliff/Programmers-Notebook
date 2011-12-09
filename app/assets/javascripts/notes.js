805cca6e9ad4c34504bc141c1cb97f8430539e64
Scott Radcliff <radcliffsc@gmail.com>
2011-12-08 21:02:52 -0500

    Refactoring JavaScript
    The DOM insertion is all wrong. I should be replacing content
    instead of entire elements

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,27 +1,48 @@
 // Notes.js
 
 $(document).ready(function() {
- // When the title span is clicked
- // replace it with a textbox
- //
- // when the textbox loses focus
- // change it back to text
- //
- // when the textarea gains focus
- // remove the default text and
- // allow text to be entered
- //
- // When the form gets submitted
- // send the new record via ajax
-  var $note_content = $("#note_content");
+  var note_content = document.getElementById("note_content");
+  var note_title = document.getElementById("note_title"); 
   var place_holder_text = "I Just Do ...";
-  $note_content.val(place_holder_text);
-  $note_content.bind('click', function(){
-    if(this.value == place_holder_text) {
-      this.value = "";
-      this.style.color = "#000000";
-      this.style.fontStyle = "normal";
-    }
-  });
+
+  if(note_title) {
+    note_title.innerHTML = "...";
+    
+    note_title.addEventListener("click", function() {
+      var title_text_box = "<input type='text' id='note_title_text_box' />";
+      if (this.value !== '...') {
+        $(this).replaceWith(title_text_box);
+        var $note_title_text_box = $("#note_title_text_box");
+        $note_title_text_box.focus();
+        document.getElementById("note_title_text_box").addEventListener("blur", createTitle);
+      };
+    });
   
+  }
+
+  if(note_content) {
+    note_content.value = place_holder_text;
+   
+    note_content.addEventListener("focus", function() {
+      if(this.value == place_holder_text) {
+        this.value = "";
+        this.style.color = "#000000";
+        this.style.fontStyle = "normal";
+      }
+    });  
+  } 
+
+  document.getElementById("note_form_submit").addEventListener("click", sendForm);
+
 });
+
+var sendForm = function(){console.log(note_title.value)};
+
+ var createTitle = function() {
+  var element = document.createElement("span");
+  var id = document.createAttribute('id');
+  id.nodeValue = "note_title";
+  element.setAttributeNode(id);
+  element.innerHTML = this.value;
+  this.parentNode.replaceChild(element, this);
+ };
